afa6f99d206a33f90f4e098cb0808f44e64d9f60
Ankit <ankit.dayal1992@gmail.com>
2023-04-15 22:56:29 +0530

    Project Setup

---
+++ src/main/java/com/ankitkumar/shoppitrest/ShoppitRestApplication.java
@@ -0,0 +1,13 @@
+package com.ankitkumar.shoppitrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppitRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppitRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankitkumar/shoppitrest/ShoppitRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankitkumar.shoppitrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppitRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
